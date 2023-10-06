39385e4249011b29f0a345b2e03e5c9cfe8e45ba
Wadi Wahyudin <wadi.wahyudin7@gmail.com>
2023-10-06 11:06:49 +0700

    add display data script

---
+++ Game/Assets/_Projects/Scripts/Server/Get Data/DisplayUserData.cs
@@ -0,0 +1,27 @@
+using Metaversitas.User;
+using UnityEngine;
+using TMPro;
+
+public class DisplayUserData : MonoBehaviour
+{
+    public App app;
+
+    [SerializeField] private TMP_Text fullName;
+    [SerializeField] private TMP_Text nim;
+    [SerializeField] private TMP_Text facultyid;
+    [SerializeField] private TMP_Text prodi;
+
+    void Start()
+    {
+        app = App.FindInstance();
+
+        // Get the user data.
+        UserData userData = app.get_userManager().get_userData();
+
+        // Convert the user data to TMPro.TMP_Text objects.
+        fullName.SetText(userData.get_fullName());
+        nim.SetText(userData.get_userUniversityID().ToString());
+        facultyid.SetText(userData.get_facultyID().ToString());
+        prodi.SetText(userData.get_facultyName());
+    }
+}
