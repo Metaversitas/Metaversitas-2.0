554fa429c2b4d50ac5846f4a9597da9a3c714bfa
RWeebz <erdhamz@gmail.com>
2023-11-09 01:11:42 +0700

    forgot to add a down migrations for trigger

--- Web/backend/migrations/20231108173145_create_trigger_on_every_updated_at_table.down.sql
+++ Web/backend/migrations/20231108173145_create_trigger_on_every_updated_at_table.down.sql
@@ -1,1 +1,6 @@
 -- Add down migration script here
+DROP TRIGGER auto_update_classMeeting_modified_timestamp on class_meeting;
+DROP TRIGGER auto_update_exams_modified_timestamp on exams;
+DROP TRIGGER auto_update_game_modified_timestamp on game;
+DROP TRIGGER auto_update_users_modified_timestamp on users;
+DROP FUNCTION auto_change_updated_at_column();
