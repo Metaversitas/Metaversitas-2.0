eda4f0c77cdbe111d7b5528eb9003353b37ef28a
Achdiyat <diyatfadhillah@gmail.com>
2023-11-09 14:40:16 +0700

    Teleport Proto Part 2

--- Game/Assets/_Projects/Scripts/Gameplay/Player.cs
+++ Game/Assets/_Projects/Scripts/Gameplay/Player.cs
@@ -11,7 +11,7 @@ using UnityEngine;
 public class Player : NetworkBehaviour
 {
 	[SerializeField] public Character CharacterPrefab;
-	
+
 	[Networked] public NetworkString<_32> Name { get; set; }
 	[Networked] public Color Color { get; set; }
 	[Networked] public NetworkBool Ready { get; set; }
@@ -19,7 +19,7 @@ public class Player : NetworkBehaviour
 
 	public bool InputEnabled => _app.AllowInput;
 
-	private Character _character;
+	public Character _character;
 	private App _app;
 
 	public override void Spawned()
@@ -31,7 +31,7 @@ public class Player : NetworkBehaviour
 
 	public override void FixedUpdateNetwork()
 	{
-		if (HasStateAuthority && _character == null && _app!=null && _app.Session!=null && _app.Session.Map)
+		if (HasStateAuthority && _character == null && _app != null && _app.Session != null && _app.Session.Map)
 		{
 			Debug.Log($"Spawning avatar for player {Name} with input auth {Object.InputAuthority}");
 			Transform t = _app.Session.Map.GetSpawnPoint(Object.InputAuthority);
@@ -43,6 +43,8 @@ public class Player : NetworkBehaviour
 			});
 		}
 	}
+
+	public Character GetCharacter() { return _character; }
 	
 	public void Despawn()
 	{
