7730273cae0def9fd6daa575270f6726d79ea608
RWeebz <erdhamz@gmail.com>
2023-11-16 13:58:36 +0700

    changing hompeage to be response

--- Web/backend/src/controllers/homepage.rs
+++ Web/backend/src/controllers/homepage.rs
@@ -1,7 +1,13 @@
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
 use axum::Json;
-use serde_json::{json, Value};
+use serde_json::{json};
 
 pub const HOMEPAGE_PATH_CONTROLLER: &str = "/";
-pub async fn homepage() -> Result<Json<Value>, ()> {
-    Ok(Json(json!({"status": true, "message": "Welcome aboard!"})))
+pub async fn homepage() -> Result<Response, ()> {
+    Ok((
+        StatusCode::OK,
+        Json(json!({"status": true, "message": "Welcome aboard!"})),
+    )
+        .into_response())
 }
